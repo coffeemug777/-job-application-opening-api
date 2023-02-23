dd534d343cfaef209a85660ab3d2dfaf9f53f8df
Denny Wibowo <indocoffee@gmail.com>
2023-02-23 17:33:41 -0500

    initial commit

---
+++ src/main/java/com/jobapplicationapp/openingapi/OpeningapiApplication.java
@@ -0,0 +1,13 @@
+package com.jobapplicationapp.openingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpeningapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpeningapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobapplicationapp/openingapi/OpeningapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobapplicationapp.openingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpeningapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
