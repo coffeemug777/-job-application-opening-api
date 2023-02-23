e1b4a20afb6b11661cfde0c37f302f526d2f6f8c
Denny Wibowo <indocoffee@gmail.com>
2023-02-23 17:57:47 -0500

    Add service to add and getall opening

---
+++ src/main/java/com/jobapplicationapp/openingapi/service/OpeningService.java
@@ -0,0 +1,31 @@
+package com.jobapplicationapp.openingapi.service;
+
+import com.jobapplicationapp.openingapi.dto.OpeningRequest;
+import com.jobapplicationapp.openingapi.model.ApplicationConnector;
+import com.jobapplicationapp.openingapi.model.Opening;
+import com.jobapplicationapp.openingapi.repository.OpeningRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class OpeningService {
+
+    @Autowired
+    private OpeningRepository openingRepository;
+
+    public Opening addOpening(OpeningRequest openingRequest) {
+        return this.openingRepository.save(Opening.builder()
+                        .title(openingRequest.getTitle())
+                        .description(openingRequest.getDescription())
+                        .completedApplications(Collections.<ApplicationConnector>emptyList())
+                        .incompleteApplications(Collections.<ApplicationConnector>emptyList())
+                .build());
+    }
+
+    public List<Opening> getAll() {
+        return this.openingRepository.findAll();
+    }
+}
