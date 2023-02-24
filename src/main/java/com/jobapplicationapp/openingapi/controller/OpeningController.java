989c4c854ad6a17348feb0fa0ad8aca1afe525c8
Denny Wibowo <indocoffee@gmail.com>
2023-02-23 20:16:40 -0500

    Switch cross origin to be before mapping, just irks me..

--- src/main/java/com/jobapplicationapp/openingapi/controller/OpeningController.java
+++ src/main/java/com/jobapplicationapp/openingapi/controller/OpeningController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/")
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@RequestMapping("/api/")
 public class OpeningController {
 
     @Autowired
